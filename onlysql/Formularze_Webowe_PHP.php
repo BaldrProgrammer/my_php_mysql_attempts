3d59babc66ce62fa8b83892845eeb190db87dc3d
BaldrProgrammer <boliklevik@gmail.com>
2026-05-29 12:25:02 +0200

    FW php connect

--- onlysql/Formularze_Webowe_PHP.php
+++ onlysql/Formularze_Webowe_PHP.php
@@ -1,1 +1,11 @@
 <?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "classicmodels";
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+if (!$conn){
+    die('blad polaczenia' . mysqli_connect_error());
+}
