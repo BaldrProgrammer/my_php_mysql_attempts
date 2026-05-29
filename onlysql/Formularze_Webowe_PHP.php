6c2c0fd0c67cd111ce663856d347449c8d096960
BaldrProgrammer <boliklevik@gmail.com>
2026-05-29 12:50:28 +0200

    FW php statements are prepared

--- onlysql/Formularze_Webowe_PHP.php
+++ onlysql/Formularze_Webowe_PHP.php
@@ -9,3 +9,12 @@ $conn = mysqli_connect($host, $username, $password, $dbname);
 if (!$conn){
     die('blad polaczenia' . mysqli_connect_error());
 }
+
+
+$orderNumber = rand(0, 999999);
+$orderDate = $_POST['orderDate'];
+$requiredDate = $orderDate + 7;
+$shippedDate = $requiredDate + 7;
+$status = $_POST['status'];
+$comments = "";
+$customerNumber = $_POST['customerNumber'];
